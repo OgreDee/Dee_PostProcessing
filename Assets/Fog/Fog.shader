fa867b1f554ef61d7bd76cd829c3074ee55e066a
zhangchuandi <zcd1990917@126.com>
2018-12-11 14:09:18 +0800

    完成Fog

--- Assets/Fog/Fog.shader
+++ Assets/Fog/Fog.shader
@@ -1,4 +1,4 @@
-﻿Shader "Unlit/Fog"
+﻿Shader "Dee/PostProcessing/Fog"
 {
 	Properties
 	{
@@ -14,8 +14,7 @@
 			CGPROGRAM
 			#pragma vertex vert
 			#pragma fragment frag
-			// make fog work
-			#pragma multi_compile_fog
+			#pragma multi_compile __ FOG_EXP_MODE FOG_LINEAR_MODE
 			
 			#include "UnityCG.cginc"
 
@@ -27,30 +26,75 @@
 
 			struct v2f
 			{
-				float2 uv : TEXCOORD0;
-				UNITY_FOG_COORDS(1)
+				float4 uv : TEXCOORD0;
 				float4 vertex : SV_POSITION;
+				float4 ray : TEXCOORD1;
 			};
 
 			sampler2D _MainTex;
 			float4 _MainTex_ST;
+			float4 _MainTex_TexelSize;
+
+			uniform sampler2D _CameraDepthTexture;
+			uniform float4x4 _Rays;
+			uniform float _FogDensity;
+			uniform float4 _FogColor;
+			uniform float _FogStart;
+			uniform float _FogEnd;
+
+
 			
 			v2f vert (appdata v)
 			{
 				v2f o;
 				o.vertex = UnityObjectToClipPos(v.vertex);
-				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-				UNITY_TRANSFER_FOG(o,o.vertex);
+				o.uv.xy = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv.zw = o.uv.xy;
+
+				int index = 0;
+				if(v.vertex.x < 0.5 && v.vertex.y < 0.5)
+				{
+					index = 0;
+				}
+				else if(v.vertex.x > 0.5 && v.vertex.y < 0.5)
+				{
+					index = 1;
+				}
+				else if(v.vertex.x > 0.5 && v.vertex.y > 0.5)
+				{
+					index = 2;
+				}
+				else
+				{
+					index = 3;
+				}
+
+				#if UNITY_UV_STARTS_AT_TOP
+				if(_MainTex_TexelSize.y < 0) //因为是一张图片，只有四个顶点，所以使用if不会有性能损耗
+				{
+					o.uv.w = 1 - o.uv.w;
+					index = 3 - index;
+				}
+				#endif
+
+				o.ray = _Rays[index];
+
 				return o;
 			}
 			
 			fixed4 frag (v2f i) : SV_Target
 			{
-				// sample the texture
+				float depth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv.zw));
+				float3 worldPos = _WorldSpaceCameraPos.xyz + depth * i.ray.xyz;
+				float v = (_FogEnd - worldPos.y) / (_FogEnd - _FogStart);
+				#if FOG_EXP_MODE
+				float factor = exp(-_FogDensity * worldPos.y);
+				#else 
+				float factor = saturate(v * _FogDensity);
+				#endif
+
 				fixed4 col = tex2D(_MainTex, i.uv);
-				// apply fog
-				UNITY_APPLY_FOG(i.fogCoord, col);
-				return col;
+				return lerp(col, _FogColor, saturate(factor));
 			}
 			ENDCG
 		}
--- Assets/Fog/PostProcessingFog.cs
+++ Assets/Fog/PostProcessingFog.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
 public class PostProcessingFog : MonoBehaviour 
 {
+    public enum Fog
+    {
+        Linear,
+        EXP
+    }
+
+    [SerializeField]
+    Shader curShader;
+
+    [SerializeField]
+    Fog fogMode;
     [SerializeField, Range(0f, 3f)]
     float fogDensity = 1.0f;
     [SerializeField]
@@ -14,43 +27,75 @@ public class PostProcessingFog : MonoBehaviour
     float fogEnd = 0f;
 
     Material mat;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+    
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        if (GetMaterial() == null)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        if(cam.depthTextureMode != DepthTextureMode.Depth)
+        {
+            cam.depthTextureMode = DepthTextureMode.Depth;
+        }
+
         mat.SetMatrix("_Rays", createRay());
         mat.SetColor("_FogColor", fogColor);
         mat.SetFloat("_FogDensity", fogDensity);
+        mat.SetFloat("_FogStart", fogStart);
+        mat.SetFloat("_FogEnd", fogEnd);
+        switch(fogMode)
+        {
+            case Fog.Linear:
+                {
+                    mat.EnableKeyword("FOG_LINEAR_MODE");
+                    mat.DisableKeyword("FOG_EXP_MODE");
+                }
+                break;
+            case Fog.EXP:
+                {
+                    mat.DisableKeyword("FOG_LINEAR_MODE");
+                    mat.EnableKeyword("FOG_EXP_MODE");
+                }
+                break;
+        }
+
+        Graphics.Blit(source, destination, mat);
     }
 
-    Camera camera;
+    Camera cam;
     Transform _CameraTransform;
-    Transform cacheCamTrans { get { return _CameraTransform ?? (camera == null ? null : camera.transform); }}
+    Transform cacheCamTrans;
+    //Transform cacheCamTrans { get { return _CameraTransform = _CameraTransform ?? (cam == null ? null : cam.transform); }}
 
     Matrix4x4 createRay()
     {
+        if(cam == null)
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        if(cacheCamTrans == null)
+        {
+            cacheCamTrans = cam.transform;
+        }
+        //cam = cam ?? GetComponent<Camera>();
+
         Matrix4x4 rayMatrix = new Matrix4x4();
 
-        float nearHight = camera.nearClipPlane *  Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float nearHight = cam.nearClipPlane *  Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
         Vector3 nearTop = cacheCamTrans.up * nearHight;
-        Vector3 nearRight = cacheCamTrans.right * nearHight * camera.aspect;
+        Vector3 nearRight = cacheCamTrans.right * nearHight * cam.aspect;
 
         Vector3 bottomLeft = cacheCamTrans.forward - nearTop - nearRight;
         Vector3 bottomRight = cacheCamTrans.forward - nearTop + nearRight;
         Vector3 topLeft = cacheCamTrans.forward + nearTop - nearRight;
         Vector3 topRight = cacheCamTrans.forward + nearTop + nearRight;
 
-        float mapValue = bottomLeft.magnitude / camera.nearClipPlane;
+        float mapValue = bottomLeft.magnitude / cam.nearClipPlane;
 
         bottomLeft.Normalize();
         bottomLeft *= mapValue;
@@ -66,9 +111,23 @@ public class PostProcessingFog : MonoBehaviour
 
         rayMatrix.SetRow(0, bottomLeft);
         rayMatrix.SetRow(1, bottomRight);
-        rayMatrix.SetRow(2, topLeft);
-        rayMatrix.SetRow(3, topRight);
+        rayMatrix.SetRow(2, topRight);
+        rayMatrix.SetRow(3, topLeft);
 
         return rayMatrix;
+    }
+
+    Material GetMaterial()
+    {
+        if (curShader == null || !curShader.isSupported)
+            return null;
+
+        if (mat == null || mat.shader != curShader)
+        {
+            mat = new Material(curShader);
+            mat.hideFlags = HideFlags.DontSave;
+        }
+
+        return mat;
     }
 }
